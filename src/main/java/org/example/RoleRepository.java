753eb710c2014f1febaf2feff6d834ff50c6678b
doktormatte <doktormatte@googlemail.com>
2024-01-20 13:31:54 +0100

    new concept

---
+++ src/main/java/org/example/RoleRepository.java
@@ -0,0 +1,8 @@
+package org.example;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String name);
+}
